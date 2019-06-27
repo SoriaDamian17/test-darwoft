d229d12a2ee3d538580f22b4d1920f67c00c2a80
Damian Soria <damian.soria.web@gmail.com>
2019-06-27 07:31:31 -0300

    fix: changue order page

--- src/app/views/order/order.module.ts
+++ src/app/views/order/order.module.ts
@@ -3,11 +3,27 @@ import { CommonModule } from '@angular/common';
 import { CheckoutComponent } from './checkout/checkout.component';
 import { ShippingComponent } from './shipping/shipping.component';
 import { PayComponent } from './pay/pay.component';
+import { SharedModule } from 'src/app/shared/shared.module';
+import { Routes } from '@angular/router';
+import { ConfirmComponent } from './confirm/confirm.component';
+
+const routes: Routes = [
+  { path: '', component: CheckoutComponent },
+  { path: 'shipping', component: ShippingComponent },
+  { path: 'pay', component: PayComponent },
+  { path: 'confirm', component: ConfirmComponent },
+];
 
 @NgModule({
-  declarations: [CheckoutComponent, ShippingComponent, PayComponent],
+  declarations: [
+    CheckoutComponent,
+    ShippingComponent,
+    PayComponent,
+    ConfirmComponent
+  ],
   imports: [
-    CommonModule
+    CommonModule,
+    SharedModule
   ]
 })
 export class OrderModule { }
